88b87f0b05fab0862f3e819936d96a33831df25f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-08 16:00:22 +0300

    tabGesture

--- ImplementDoubleTapAndPinchToZoom/ContentView.swift
+++ ImplementDoubleTapAndPinchToZoom/ContentView.swift
@@ -10,11 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State var scale: CGFloat = 1.0
+    @State var isTapped: Bool = false
     var body: some View {
         Image("pizza")
             .resizable()
             .scaledToFit()
             .scaleEffect(self.scale)
+            .scaleEffect(self.isTapped ? 2 : 1, anchor: UnitPoint(x: 0, y: 0))
+            .gesture(TapGesture(count: 2).onEnded({
+                self.isTapped = !self.isTapped
+            }).simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged({ (value) in
+                print(value.startLocation)
+            }).onEnded({ (value) in
+                print(value.location)
+            })))
             .gesture(MagnificationGesture().onChanged({ (scsle) in
                 self.scale = self.scale.magnitude
             }).onEnded({ (scaleFinal) in
