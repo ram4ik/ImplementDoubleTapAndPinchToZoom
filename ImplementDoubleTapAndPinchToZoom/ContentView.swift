9120ea4ca89311d3feb41ca7fab403f1869a9f86
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-08 16:40:07 +0300

    renaming

--- ImplementDoubleTapAndPinchToZoom/ContentView.swift
+++ ImplementDoubleTapAndPinchToZoom/ContentView.swift
@@ -12,24 +12,25 @@ struct ContentView: View {
     @State var scale: CGFloat = 1.0
     @State var isTapped: Bool = false
     @State var pointTapped: CGPoint = CGPoint.zero
-    @State var draggedWidth: CGSize = CGSize.zero
-    @State var previousDRagged: CGSize = CGSize.zero
+    @State var draggedSize: CGSize = CGSize.zero
+    @State var previousDragged: CGSize = CGSize.zero
     var body: some View {
         GeometryReader { reader in
             Image("pizza")
                 .resizable()
                 .scaledToFit()
                 .animation(.default)
+                .offset(x: self.draggedSize.width, y: self.draggedSize.height)
                 .scaleEffect(self.scale)
                 .scaleEffect(self.isTapped ? 2 : 1, anchor: UnitPoint(x: self.pointTapped.x / reader.frame(in: .global).maxX, y: self.pointTapped.y / reader.frame(in: .global).maxY))
                 .gesture(TapGesture(count: 2).onEnded({
                     self.isTapped = !self.isTapped
                 }).simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged({ (value) in
                     self.pointTapped = value.startLocation
-                    self.draggedWidth = CGSize(width: value.translation.width + self.previousDRagged.width, height: value.translation.height + self.previousDRagged.height)
+                    self.draggedSize = CGSize(width: value.translation.width + self.previousDragged.width, height: value.translation.height + self.previousDragged.height)
                 }).onEnded({ (value) in
-                    self.draggedWidth = CGSize(width: value.translation.width + self.previousDRagged.width, height: value.translation.height + self.previousDRagged.height)
-                    self.previousDRagged = self.draggedWidth
+                    self.draggedSize = CGSize(width: value.translation.width + self.previousDragged.width, height: value.translation.height + self.previousDragged.height)
+                    self.previousDragged = self.draggedSize
                 })))
                 .gesture(MagnificationGesture().onChanged({ (scsle) in
                     self.scale = self.scale.magnitude
