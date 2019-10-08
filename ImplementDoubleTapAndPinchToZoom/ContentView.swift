f83b2204c9f4fb4f2b1247d1b2a1dfa4b9127142
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-08 16:14:08 +0300

    added animation

--- ImplementDoubleTapAndPinchToZoom/ContentView.swift
+++ ImplementDoubleTapAndPinchToZoom/ContentView.swift
@@ -11,24 +11,28 @@ import SwiftUI
 struct ContentView: View {
     @State var scale: CGFloat = 1.0
     @State var isTapped: Bool = false
+    @State var pointTapped: CGPoint = CGPoint.zero
     var body: some View {
-        Image("pizza")
-            .resizable()
-            .scaledToFit()
-            .scaleEffect(self.scale)
-            .scaleEffect(self.isTapped ? 2 : 1, anchor: UnitPoint(x: 0, y: 0))
-            .gesture(TapGesture(count: 2).onEnded({
-                self.isTapped = !self.isTapped
-            }).simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged({ (value) in
-                print(value.startLocation)
-            }).onEnded({ (value) in
-                print(value.location)
-            })))
-            .gesture(MagnificationGesture().onChanged({ (scsle) in
-                self.scale = self.scale.magnitude
-            }).onEnded({ (scaleFinal) in
-                self.scale = scaleFinal.magnitude
-            }))
+        GeometryReader { reader in
+            Image("pizza")
+                .resizable()
+                .scaledToFit()
+                .animation(.default)
+                .scaleEffect(self.scale)
+                .scaleEffect(self.isTapped ? 2 : 1, anchor: UnitPoint(x: self.pointTapped.x / reader.frame(in: .global).maxX, y: self.pointTapped.y / reader.frame(in: .global).maxY))
+                .gesture(TapGesture(count: 2).onEnded({
+                    self.isTapped = !self.isTapped
+                }).simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged({ (value) in
+                    self.pointTapped = value.startLocation
+                }).onEnded({ (value) in
+                    print(value.location)
+                })))
+                .gesture(MagnificationGesture().onChanged({ (scsle) in
+                    self.scale = self.scale.magnitude
+                }).onEnded({ (scaleFinal) in
+                    self.scale = scaleFinal.magnitude
+                }))
+        }
     }
 }
 
